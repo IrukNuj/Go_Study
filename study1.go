64f562696c5dc1ca237a19c58fc377df4b9fb909
IrukNuj <b1716568@planet.kanazawa-it.ac.jp>
2018-11-22 12:06:53 +0900

    add comment

--- study1.go
+++ study1.go
@@ -3,13 +3,13 @@ package main
 import "fmt"
 
 func main()  {
-	var num int
+	var num int //型は後ろに置く
 	var pow int
 	var result = 1
 
 	num = 2
 	pow = 4
-	for  i:= 0; i < pow ; i++ {
+	for  i:= 0; i < pow ; i++ { //波括弧は必須
 		result *= num
 	}
 
